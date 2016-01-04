efe00987d7fe10c80c71260e14d393ac65842483
Pedro Pinheiro <pdpinheiro@gmail.com>
2016-01-04 14:45:19 +0000

    Fix v2 address handling

--- php/callback.php
+++ php/callback.php
@@ -5,7 +5,12 @@ include 'include.php';
 $db = new mysqli($mysql_host, $mysql_username, $mysql_password) or die(__LINE__ . ' Invalid connect: ' . mysqli_error());
 $db->select_db($mysql_database) or die( "Unable to select database. Run setup first.");
 
-$stmt = $db->prepare("select price_in_usd, product_url, price_in_btc from invoices where invoice_id = ?");
+$invoice_id = $_GET['invoice_id'];
+$transaction_hash = $_GET['transaction_hash'];
+$value_in_btc = $_GET['value'] / 100000000;
+
+
+$stmt = $db->prepare("select address from invoices where invoice_id = ?");
 $stmt->bind_param("i",$invoice_id);
 $success = $stmt->execute();
 
@@ -15,25 +20,13 @@ if (!$success) {
 
 $result = $stmt->get_result();
 while($row = $result->fetch_array()) {
-  $product_url = $row['product_url'];
-  $price_in_usd = $row['price_in_usd'];
-  $price_in_btc = $row['price_in_btc'];
+  $my_address = $row['address'];
 }
 
 $result->close();
 $stmt->close(); 
 
-$invoice_id = $_GET['invoice_id'];
-$transaction_hash = $_GET['transaction_hash'];
-$value_in_btc = $_GET['value'] / 100000000;
-
-//Commented out to test, uncomment when live
-if (array_key_exists('test', $_GET) and $_GET['test'] == true) {
-  echo 'Ignoring Test Callback';
-  return;
-}
-
-if ($_GET['address'] != $my_bitcoin_address) {
+if ($_GET['address'] != $my_address) {
     echo 'Incorrect Receiving Address';
   return;
 }
